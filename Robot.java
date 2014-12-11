d2be912dd4d0d94e19585562ab9d4fefc7bbc98d
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2014-12-11 13:28:10 +0000

    Tidy detection into method

--- Robot.java
+++ Robot.java
@@ -2,31 +2,40 @@ import icommand.nxt.*;
 import icommand.nxt.comm.NXTCommand;
 
 public class Robot {
+	private final static int LIGHT_THESHOLD = 550;
+	private final static SensorPort SENSOR_PORT = SensorPort.S2;
+	private static LightSensor lineDetect;
+
+	public static boolean blackDetected() {
+		//LightSensor lineDetect = new LightSensor(SensorPort.S2);
+
+		return lineDetect.getLightValue() < LIGHT_THESHOLD;
+	}
+
+	public static void playTone(int duration) {
+		Sound.playTone(500, duration);
+	}
+
 	public static void main(String[] args) throws InterruptedException {
-		final int INTERVAL = 100;
-		final int LIGHT_THESHOLD = 250;
+		final int INTERVAL = 40;
 
 		NXTCommand.open();
 		NXTCommand.setVerify(true);
+		lineDetect = new LightSensor(SENSOR_PORT);
 
-		LightSensor lineDetect = new LightSensor(SensorPort.S2	);
-		lineDetect.activate();
+		System.out.println("Robot control started.");
+			// lineDetect.activate();
 		// lineDetect.passivate();
 
-		int lightValue = 24;
 		while( true ) {
-
-
 			// ColourSensor lineDetect = new ColorSensor(SENSOR_PORT);
 			// statements to control the robot
 			//
 			//Motor.B.backward();
 			//Motor.B.setSpeed(900);
-			lightValue = lineDetect.getLightValue();
-			System.out.println(lightValue);
-			if( lightValue < LIGHT_THESHOLD ) {
-				System.out.println("BLACK DETECTED!!!");
-				Sound.playTone(500, INTERVAL);
+			if( blackDetected() ) {
+				playTone(INTERVAL);
+				System.out.println("Black detected.");
 			}
 			Thread.sleep(INTERVAL);
 		}
