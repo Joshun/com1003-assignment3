d66251fb7d9de45b29b07361f0d04028f6ce8805
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2015-01-21 15:23:22 +0000

    Remove unneeded stuff

--- README.md
+++
@@ -1,4 +0,0 @@
-com1003-assignment3
-===================
-
-COM1003 Group Assignment
--- generate_javadoc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm doc/*
-javadoc -cp icommand.jar *.java -verbose
-mv *.html *.css *.js package-list doc
